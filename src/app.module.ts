c96a0375f6f66e33d7478f240d515e4c33620b0a
FedeLupianez <lupianezfederico@gmail.com>
2026-06-10 09:27:13 -0300

    admins entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'data-source';
 import { UsersModule } from './entities/users/users.module';
 import { CompaniesModule } from './entities/companies/companies.module';
+import { AdminsModule } from './entities/admins/admins.module';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(dataSourceOptions),
         UsersModule,
-        CompaniesModule
+        CompaniesModule,
+        AdminsModule
     ],
     controllers: [AppController],
     providers: [AppService],
---
+++ src/entities/admins/admins.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { CompaniesEntity } from "../companies/companies.entity";
+
+
+@Entity('Admins')
+export class AdminsEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id_admin: string;
+
+    @Column({ type: 'uuid', name: 'id_company' })
+    id_company: string;
+
+    @ManyToOne(() => CompaniesEntity, { nullable: false })
+    @JoinColumn({ name: 'id_company', referencedColumnName: 'id_company' })
+    company: CompaniesEntity;
+
+    @Column({ type: 'varchar', length: 50, name: 'email', nullable: false })
+    email: string;
+
+    @Column({ type: 'varchar', length: 255, name: 'password', nullable: false })
+    password: string;
+}
