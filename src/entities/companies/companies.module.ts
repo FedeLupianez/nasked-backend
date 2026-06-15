3b0dd7588ae13c53c41048639c163d5050d7475a
FedeLupianez <lupianezfederico@gmail.com>
2026-06-15 15:58:34 -0300

    fix dependency error with company

--- src/entities/companies/companies.module.ts
+++ src/entities/companies/companies.module.ts
@@ -3,10 +3,14 @@ import { UserModule } from 'src/user/user.module';
 import { AdminGuard } from 'src/auth/admin.guard';
 import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CompaniesEntity } from './companies.entity';
 
 @Module({
-  imports: [UserModule],
-  providers: [CompaniesService, AdminGuard],
-  controllers: [CompaniesController]
+    imports: [UserModule, TypeOrmModule.forFeature([CompaniesEntity])],
+    providers: [CompaniesService, AdminGuard],
+    controllers: [CompaniesController],
+    exports: [CompaniesService]
+
 })
-export class CompaniesModule {}
+export class CompaniesModule { }
